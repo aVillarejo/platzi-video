96ef20cdcbadb88268f4fd7ae56c7f2b3f0117f1
“a_villarejo19” <a.villarejo19@gmail.com>
2018-10-14 13:51:37 -0500

    Imprimiendo datos del estado

--- redux.js
+++ redux.js
@@ -35,7 +35,18 @@ const initialState = [
 ];
 
 const store = createStore(
-  (state) => state,
+  state => state,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const $container = document.getElementById("playlist");
+const playlist = store.getState();
+
+playlist.forEach(item => {
+  const template = document.createElement("p");
+  template.textContent = item.title;
+  $container.appendChild(template);
+});
+
+console.log(store.getState());
